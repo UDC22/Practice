6d026e134b0f8e15a5ac922dbacce6a3e0b40c67
UDC22 <99418963+UDC22@users.noreply.github.com>
2022-02-10 23:21:30 +0700

    Add files via upload

---
+++ Assets/Controller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 10f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(0, 14f);
+        }
+    }
+}
